2d7d05263ec57e68415b7ff267dbdee4a72b12a2
bala-amypo <bala@amypo.in>
2025-12-21 16:39:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,15 @@
-package com.example.project.controller;
+package com.example.demo.controller;
 
-import com.example.project.entity.User;
-import com.example.project.service.UserService;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User Controller")
 public class UserController {
 
     private final UserService userService;
@@ -18,12 +20,12 @@ public class UserController {
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
-        return userService.register(user);
+        return userService.registerUserEntity(user);
     }
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id) {
-        return userService.findById(id);
+        return userService.getUserById(id);
     }
 
     @GetMapping
